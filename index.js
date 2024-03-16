bf2ff0d00d2d71aea9f0eca75df4c694f02ca851
Jennifer Valor <profe.jennifer.valor@gmail.com>
2024-03-15 21:21:23 -0300

    configuramos un puerto para el repo

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import express from 'express'
 console.log('Bienvenidos C74i lo mejor de rolling')
 
 // 1- configurar un puerto 
+const app = express();
 
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+ app.get('port'));  
+})
 // 2- configurar middlewares
 
 // 3- configuración de las rutas
