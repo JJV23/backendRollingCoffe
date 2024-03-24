433c3acd3bcb76ed6795efe75cde7559227b519b
Jennifer Valor <profe.jennifer.valor@gmail.com>
2024-03-24 18:22:57 -0300

    agregamos el controlador crearProducto con la logica necesaria para generar un nuevo producto en la BD

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,5 +1,28 @@
+import Producto from "../database/models/producto.js" 
+
 export const listarProductos =(req, res)=>{
     console.log('hola comisión')
     res.send('aquí listar los productos')
 
+}
+export const crearProductos = async(req, res)=>{
+    try {
+   //extraer los datos del body
+   //console.log(req);
+   console.log(req.body);
+   //todo: agregar a la validacion de los datos del body
+   const productoNuevo = new Producto(req.body);
+   //pedirle a la BD guardar el producto nuevo
+   await productoNuevo.save(); 
+   //enviar la respuesta al front
+   // res.send('aquí creo el producto')
+   res.status(201).json({
+    mensaje: "El producto fue creado correctamente"
+   })
+} catch (error) {
+    console.log(error);
+    res.status(400).json({
+        mensaje: "El producto no pudo ser dado de alta"
+    })
+}
 }
