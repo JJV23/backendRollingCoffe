294ba2fab5ab3dc3d172637665645b1d6b9340d6
Jennifer Valor <profe.jennifer.valor@gmail.com>
2024-03-25 00:39:52 -0300

    agregamos una funcion para obtenerun producto mediante su id

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,28 +1,49 @@
-import Producto from "../database/models/producto.js" 
+import Producto from "../database/models/producto.js";
 
-export const listarProductos =(req, res)=>{
-    console.log('hola comisión')
-    res.send('aquí listar los productos')
-
-}
-export const crearProductos = async(req, res)=>{
-    try {
-   //extraer los datos del body
-   //console.log(req);
-   console.log(req.body);
-   //todo: agregar a la validacion de los datos del body
-   const productoNuevo = new Producto(req.body);
-   //pedirle a la BD guardar el producto nuevo
-   await productoNuevo.save(); 
-   //enviar la respuesta al front
-   // res.send('aquí creo el producto')
-   res.status(201).json({
-    mensaje: "El producto fue creado correctamente"
-   })
-} catch (error) {
-    console.log(error);
+export const listarProductos = async (req, res) => {
+  try {
+    const productos = await Producto.find();
+    res.status(200).json(productos);
+    //console.log('hola comisión')
+    //res.send('aquí listar los productos')
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({
+      mensaje: "No se pudo obtener la lista de productos",
+    });
+  }
+};
+export const crearProductos = async (req, res) => {
+  try {
+    //extraer los datos del body
+    //console.log(req);
+    console.log(req.body);
+    //todo: agregar a la validacion de los datos del body
+    const productoNuevo = new Producto(req.body);
+    //pedirle a la BD guardar el producto nuevo
+    await productoNuevo.save();
+    //enviar la respuesta al front
+    // res.send('aquí creo el producto')
+    res.status(201).json({
+      mensaje: "El producto fue creado correctamente",
+    });
+  } catch (error) {
+    console.error(error);
     res.status(400).json({
-        mensaje: "El producto no pudo ser dado de alta"
-    })
-}
+      mensaje: "El producto no pudo ser dado de alta",
+    });
+  }
+};
+export const obtenerProducto = async(req, res)=> {
+    try{
+        //extraer el parametro id
+        console.log(req.params.id);
+        //buscar el producto en la BD
+        const productoBuscado = await Producto.findById(req.params.id);
+        //responder con el producto
+        res.status(200).json(productoBuscado);
+    }catch(error){
+        console.error(error)
+        res.status(404).json({mensaje:"No se encontro el producto buscado"})
+    }
 }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { crearProductos, listarProductos } from "../controllers/productos.controllers.js";
+import { crearProductos, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 router.route('/productos').get(listarProductos).post(crearProductos)
+router.route('/productos/:id').get(obtenerProducto) 
 
 export default router;
